0147703b325b4125569c34183d89f87e685b3637
medtaher <medtaherbelkadhi@gmail.com>
2026-03-13 02:48:55 +0100

    merge codepc1

--- chaine.py
+++ chaine.py
@@ -1,5 +1,9 @@
-
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-X = np.array([[10,20],[15,30],[20,40],[25,50]]) 
+X = np.array([[10,20],[15,30],[20,40],[25,50]])
+y = np.array([100,80,60,50])
+
+model = LinearRegression().fit(X, y)
+print(model.predict([[18,35]]))
+
