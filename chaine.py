4af14618b6e88dc3532465c45fee238903fc93b6
medtaher <medtaherbelkadhi@gmail.com>
2026-03-13 02:32:55 +0100

    resolve conflict

--- chaine.py
+++ chaine.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-X = np.array([[10,20],[15,30],[20,40],[25,50]]) # latence(ms), utilisateurs
-=======
+
 from sklearn.linear_model import LinearRegression
 import numpy as np
->>>>>>> codepc1
+
+X = np.array([[10,20],[15,30],[20,40],[25,50]]) 
